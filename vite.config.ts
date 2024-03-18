cd763bbac1499554303609f8e00fd748533b57d3
Borko Rajkovic <rajkovicborko@gmail.com>
2024-03-18 13:07:13 +0100

    Build options for integration with Jekyll

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import packageJson from "./package.json";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: `${
+    packageJson.baseUrl.useCustomBaseUrl
+      ? packageJson.baseUrl.baseUrlPrefix + packageJson.version
+      : ""
+  }`,
+  build: {
+    outDir: `dist/version-${packageJson.version}`,
+  },
+});
