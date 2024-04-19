7fdd74a1904839472e79a2ec0c0a720cbbde512b
Borko Rajkovic <rajkovicborko@gmail.com>
2024-04-19 10:16:55 +0200

    Remove Build options for integration with Jekyll

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from "vite";
-import packageJson from "./package.json";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  if (command === "build") {
-    return {
-      plugins: [react()],
-      base: `${
-        packageJson.baseUrl.useCustomBaseUrl
-          ? packageJson.baseUrl.baseUrlPrefix + packageJson.version
-          : ""
-      }`,
-      build: {
-        outDir: `dist/version-${packageJson.version}`,
-      },
-    };
-  } else {
-    return {
-      plugins: [react()],
-    };
-  }
+export default defineConfig({
+  plugins: [react()],
 });
