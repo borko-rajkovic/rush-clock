898c04e4eea601c299c6f3259c9fa1003c422baa
Borko Rajkovic <rajkovicborko@gmail.com>
2024-04-24 09:32:43 +0200

    Transform unicode characters to png images

--- src/components/ClockConfiguration/ColorThemeConfiguration/ColorThemeConfiguration.tsx
+++ src/components/ClockConfiguration/ColorThemeConfiguration/ColorThemeConfiguration.tsx
@@ -2,6 +2,9 @@ import { useContext } from "react";
 import "./ColorThemeConfiguration.css";
 import { FeaturesContext } from "../../../context/features-context/FeaturesContext";
 import { ColorTheme } from "./ColorTheme";
+import strawberry from "./theme-icons/strawberry.png";
+import blueberry from "./theme-icons/blueberry.png";
+import lime from "./theme-icons/lime.png";
 
 function ColorThemeConfiguration() {
   const { hueColor, setHueColor } = useContext(FeaturesContext);
@@ -19,7 +22,7 @@ function ColorThemeConfiguration() {
           onChange={() => setHueColor(ColorTheme.Cherry)}
         />
         <label className="color_theme__label" htmlFor="cherry">
-          🍓
+          <img src={strawberry} />
         </label>
         <input
           className="color_theme__input"
@@ -30,7 +33,7 @@ function ColorThemeConfiguration() {
           onChange={() => setHueColor(ColorTheme.Blueberry)}
         />
         <label className="color_theme__label" htmlFor="blueberry">
-          🫐
+          <img src={blueberry} />
         </label>
         <input
           className="color_theme__input"
@@ -41,7 +44,7 @@ function ColorThemeConfiguration() {
           onChange={() => setHueColor(ColorTheme.Lime)}
         />
         <label className="color_theme__label" htmlFor="lime">
-          🍋‍🟩
+          <img src={lime} />
         </label>
       </div>
     </>
