731d298ad7cb1718ab85b029e2926a5021408a36
Borko Rajkovic <rajkovicborko@gmail.com>
2024-05-05 19:01:45 +0200

    Separate Desktop and Mobile rendering

--- src/ScreenResolutionGuard.tsx
+++ src/ScreenResolutionGuard.tsx
@@ -2,22 +2,19 @@ import { useContext } from "react";
 import useWindowDimensions from "./hooks/use-window-dimensions-hook";
 import { FeaturesContext } from "./context/features-context/FeaturesContext";
 
-const MINIMUM_WIDTH = 1024;
+export const MINIMUM_WIDTH = 1024;
 
-const ScreenResolutionGuard = ({ children }: { children: React.ReactNode }) => {
+const ScreenResolutionGuard = ({
+  desktop,
+  mobile,
+}: {
+  desktop: React.ReactNode;
+  mobile: React.ReactNode;
+}) => {
   const { width } = useWindowDimensions();
   const { hideAdmin } = useContext(FeaturesContext);
 
-  return !hideAdmin && width < MINIMUM_WIDTH ? (
-    <div className="screen-resolution-guard">
-      <div className="screen-resolution-guard__content">
-        Minimum supported width for admin&nbsp;is&nbsp;
-        <strong>{MINIMUM_WIDTH}</strong>px
-      </div>
-    </div>
-  ) : (
-    children
-  );
+  return !hideAdmin && width < MINIMUM_WIDTH ? mobile : desktop;
 };
 
 export default ScreenResolutionGuard;
