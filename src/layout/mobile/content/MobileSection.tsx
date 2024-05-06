9ea47f41d57a6f6ef51096ae982221eae1389f47
Borko Rajkovic <rajkovicborko@gmail.com>
2024-05-06 21:23:17 +0200

    Implement How it works mobile section

--- src/layout/mobile/content/MobileSection.tsx
+++ src/layout/mobile/content/MobileSection.tsx
@@ -1,3 +1,4 @@
+import HowItWorksSection from "./sections/HowItWorksSection";
 import RushClockSection from "./sections/RushClockSection";
 
 const MOBILE_SECTIONS = [
@@ -5,6 +6,11 @@ const MOBILE_SECTIONS = [
     path: "rush-clock",
     title: "Rush Clock",
     component: <RushClockSection />,
+  },
+  {
+    path: "how-it-works",
+    title: "How it works?",
+    component: <HowItWorksSection />,
   },
 ];
 
