d313e2408fb22c549c1092fc6561d3ba9f851a75
Borko Rajkovic <rajkovicborko@gmail.com>
2024-05-07 22:44:27 +0200

    Implement PWA support for mobile devices

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,66 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      manifestFilename: "site.webmanifest",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*"],
+        runtimeCaching: [
+          {
+            urlPattern: () => {
+              return true;
+            },
+            handler: "CacheFirst" as const,
+            options: {
+              cacheableResponse: {
+                statuses: [0, 200, 206],
+              },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: "Rush Clock",
+        short_name: "Rush Clock",
+        description: "Clock that rushes",
+        theme_color: "#ffffff",
+        // screenshots: [
+        //   {
+        //     src: "/icons/android-chrome-512x512.png",
+        //     sizes: "512x512",
+        //     form_factor: "wide",
+        //   },
+        //   {
+        //     src: "/icons/android-chrome-512x512.png",
+        //     sizes: "512x512",
+        //     form_factor: "narrow",
+        //   },
+        // ],
+        icons: [
+          {
+            src: "/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
 });
